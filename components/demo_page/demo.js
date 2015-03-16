7afdf08a12975b027ff6c621a50483d77e477086
Zhenqian He <z23he@uwaterloo.ca>
2015-03-16 15:59:15 -0400

    resets the lights back to the original colors when video stops

--- components/demo_page/demo.js
+++ components/demo_page/demo.js
@@ -13,10 +13,12 @@ define([
     ])
   .controller('demoController', [
     '$scope', 
+    '$http',
     'store',
     'setupModal',
   function(
-    $scope, store, setupModal) {
+    $scope, $http, store, setupModal) {
+
     $scope.setup = function() {
       $scope.message = "connecting...";
       setupModal().then(function() {
@@ -31,9 +33,38 @@ define([
           setBridge($scope.bridge);
           setUsername($scope.userName);
           setLights($scope.lights);
+          getOriginalLightColor($scope.lights);
           $scope.message = "Successfully established connection to the hues";
         }
       });
+    }
+
+    function getOriginalLightColor(lights) {
+      var url = 'http://' + curBridge + '/api/' + username + '/lights/';
+      $scope.lightStates = {}
+
+      $http.get(url).success(function(data) {
+        for (var i = 0; i < lights.length; i++) {
+          $scope.lightStates[lights[i]] = data[lights[i]].state;
+        }
+      });
+    }
+
+    function setOriginalLightColor(lights) {
+      for (var i = 0; i < lights.length; i++) {
+        var state = $scope.lightStates[lights[i]];
+        var url = 'http://' + curBridge + '/api/' + username + '/lights/' + lights[i] + '/state';
+        $http.put(url, {
+          on: state.on,
+          bri: state.bri,
+          hue: state.hue,
+          sat: state.sat
+        }).success(function(data) {
+          if (data[0].error) {
+            console.log('unable to set light '+ lights[i]+' back');
+          }
+        })
+      }
     }
 
     if (!store.get('hrt-data')) {
@@ -47,12 +78,13 @@ define([
       setBridge($scope.bridge);
       setUsername($scope.userName);
       setLights($scope.lights);
+      getOriginalLightColor($scope.lights);
       $scope.message = "Successfully established connection to the hues";
     }
 
     $('#hrt-player').on('play',function(){
       for (var i = 0; i < lightids.length; i++) {
-        setTransitionTime(lightids[i-1], 1);
+        setTransitionTime(lightids[i], 1);
       }
       hrtInterval = setInterval(updateHrtFrame, 33);
     });
@@ -60,8 +92,9 @@ define([
     $('#hrt-player').on('pause',function(){
       // set transition time back to default
       for (var i = 0; i < lightids.length; i++) {
-        setTransitionTime(lightids[i-1], 4);
+        setTransitionTime(lightids[i], 4);
       }
+      setOriginalLightColor($scope.lights);
       clearInterval(hrtInterval)
     });
     $.get('components/demo_page/hrt/demo.hrt', function(data) {
